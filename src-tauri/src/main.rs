7dc516b46cff48d61ef1f4657d0b344219c63ddf
t4ai6i <t4ai6i>
2022-08-23 21:10:14 +0900

    Implement selecting local video file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,51 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
 
+use std::fs;
+use serde::Serialize;
+
+#[derive(Serialize)]
+#[serde(tag = "type")]
+enum Entry {
+    #[serde(rename = "file")]
+    File {
+        name: String,
+        path: String,
+    },
+    #[serde(rename = "dir")]
+    Dir {
+        name: String,
+        path: String,
+    },
+}
+
+#[tauri::command]
+fn get_entries(path: &str) -> Result<Vec<Entry>, String> {
+    let entries = fs::read_dir(path).map_err(|e| format!("{}", e))?;
+    let res: Vec<Entry> = entries
+        .filter_map(|entry| -> Option<Entry> {
+            let entry = entry.ok()?;
+            let name = entry.file_name().to_string_lossy().to_string();
+            let path = entry.path().to_string_lossy().to_string();
+            let r#type = entry.file_type().ok()?;
+
+            if r#type.is_dir() {
+                Some(Entry::Dir { name, path })
+            } else if r#type.is_file() {
+                Some(Entry::File { name, path })
+            } else {
+                None
+            }
+        })
+        .collect();
+    Ok(res)
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_entries])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,86 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import { homeDir } from "@tauri-apps/api/path";
+import { convertFileSrc } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api";
 import "./App.css";
+import { always, equals, ifElse } from "ramda";
+
+interface Entry {
+  type: "dir" | "file";
+  name: string;
+  path: string;
+}
+
+type Entries = Entry[];
 
 const App: React.FC = () => {
+  const [src, setSrc] = useState<string | null>(null);
+  const [dir, setDir] = useState<string | null>(null);
+  const [player, setPlayer] = useState<JSX.Element | null>(null);
+  const [entries, setEntries] = useState<Entries | null>(null);
+
+  useEffect(() => {
+    void (async () => {
+      const home = await homeDir();
+      setDir(home);
+    })();
+  }, []);
+
+  useEffect(() => {
+    void (() => {
+      if (src == null) {
+        return;
+      }
+      const url = convertFileSrc(src);
+      const player = <ReactPlayer url={url} controls={true} />;
+      setPlayer(player);
+    })();
+  }, [src]);
+
+  useEffect(() => {
+    void (async () => {
+      try {
+        const entries = await invoke<Entries>("get_entries", { path: dir });
+        setEntries(entries);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [dir]);
+
+  const entryList: JSX.Element | null =
+    entries != null ? (
+      <ul>
+        {entries.map((entry): JSX.Element => {
+          return ifElse(
+            equals("dir"),
+            always(
+              <li key={entry.path} onClick={() => setDir(entry.path)}>
+                {entry.name}
+              </li>
+            ),
+            always(
+              <li key={entry.path} onClick={() => setSrc(entry.path)}>
+                {entry.name}
+              </li>
+            )
+          )(entry.type);
+        })}
+      </ul>
+    ) : null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          <br />
-          <br />
-          Hello Tauri!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React Player</h1>
+      {player}
+      <br />
+      src: {src ?? "(not selected)"}
+      <br />
+      dir: {dir ?? ""}
+      <br />
+      {entryList}
+    </>
   );
 };
 
